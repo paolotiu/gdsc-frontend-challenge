965287269f4b8ad5cbad3c83e28bf6d8d2b50776
Paolo Tiu <paolotiu17@gmail.com>
2021-10-19 23:27:35 +0800

    feat: bare dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { styled } from '@mui/system';
+import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
+import { useState } from 'react';
 
-interface Props {}
+const Main = styled('main')<{ shouldHaveMargin?: boolean }>(({ shouldHaveMargin }) => ({
+  transition: 'all 225ms cubic-bezier(0, 0, 0.2, 1) 0ms',
+  marginLeft: shouldHaveMargin && '200px',
+}));
 
-const Dashboard = (props: Props) => {
-  return <div></div>;
+const Dashboard = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div>
+      <Main shouldHaveMargin={isOpen}>
+        <Header />
+      </Main>
+
+      <Sidebar isOpen={isOpen} />
+    </div>
+  );
 };
 
 export default Dashboard;
